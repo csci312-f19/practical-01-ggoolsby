0596fa0c3eae655e06e13524630c7ae3d4779273
ggoolsby <ggoolsby@middlebury.edu>
2019-09-12 15:36:47 -0400

    First birthday practical commit

---
+++ index.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-global-assign, no-underscore-dangle */
+
+const birthday = require('./index');
+
+describe('Determine age based on birthday', () => {
+  let _Date;
+  beforeAll(() => {
+    _Date = Date;
+  });
+  afterAll(() => {
+    Date = _Date;
+  });
+  beforeEach(() => {
+    Date.now = jest.fn(() => new Date('01 Jan 2018').valueOf());
+  });
+
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('01 Jan 2018'))).toBe(0);
+  });
+
+  test('Returns 1 if birthday is 01 Jan 2017', () => {
+    expect(birthday.howOld(new Date('01 Jan 2017'))).toBe(1);
+  });
+
+  test('Returns 1 if birthday is 15 Dec 2016', () => {
+    expect(birthday.howOld(new Date('15 Dec 2016'))).toBe(1);
+  });
+
+  test('Returns 1 if birthday is 15 Jan 2017', () => {
+    expect(birthday.howOld(new Date('15 Jan 2017'))).toBe(0);
+  });
+
+  test('Returns 0 if birthday is 15 Sep 2017', () => {
+    expect(birthday.howOld(new Date('15 Sep 2017'))).toBe(0);
+  });
+});
